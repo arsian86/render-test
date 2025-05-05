4e596a4faca0c98c43d7b11e9ba3cc272f69de32
Arsian Kuo <arsiankuo86@gmail.com>
2025-05-06 02:21:54 +0800

    12

--- config/db.js
+++ config/db.js
@@ -4,7 +4,6 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  synchronize: process.env.DB_SYNCHRONIZE === "true",
   ssl:
     process.env.DB_ENABLE_SSL === "true"
       ? { rejectUnauthorized: false }
