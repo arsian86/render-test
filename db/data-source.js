3dc9baaebea011e6c88a70dedd1f6d4de16f33f6
Arsian Kuo <arsiankuo86@gmail.com>
2025-05-06 01:12:26 +0800

    data-source

--- db/data-source.js
+++ db/data-source.js
@@ -1,6 +1,20 @@
 const { DataSource } = require("typeorm");
 const db_config = require("../config/db");
 
+const { Admin } = require("../entities/Admin");
+const { Coach_Skill } = require("../entities/Coach_Skill");
+const { Coach } = require("../entities/Coach");
+const { Course_Chapter } = require("../entities/Course_Chapter");
+const { Course_Video } = require("../entities/Course_Video");
+const { Course } = require("../entities/Course");
+const { Payment_Transfer } = require("../entities/Payment_Transfer");
+const { Rating } = require("../entities/Rating");
+const { Skill } = require("../entities/Skill");
+const { Subscription_Skill } = require("../entities/Subscription_Skill");
+const { Subscription } = require("../entities/Subscription");
+const { User_Course_Favorite } = require("../entities/User_Course_Favorite");
+const { User } = require("../entities/User");
+
 const AppDataSource = new DataSource({
   type: "postgres",
   host: db_config.host,
@@ -9,22 +23,22 @@ const AppDataSource = new DataSource({
   password: db_config.password,
   database: db_config.database,
   ssl: db_config.ssl,
-  synchronize: false, //開發時 true，部署時請改為 false 並使用 migration
+  synchronize: false, // 開發時 true，部署時請改為 false 並使用 migration
   logging: true,
   entities: [
-    require("../entities/Admin"),
-    require("../entities/User"),
-    require("../entities/Coach"),
-    require("../entities/Course"),
-    require("../entities/Subscription"),
-    require("../entities/Subscription_Skill"),
-    require("../entities/Rating"),
-    require("../entities/Coach_Skill"),
-    require("../entities/Skill"),
-    require("../entities/User_Course_Favorite"),
-    require("../entities/Course_Chapter"),
-    require("../entities/Payment_Transfer"),
-    require("../entities/Course_Video"),
+    Admin,
+    Coach_Skill,
+    Coach,
+    Course_Chapter,
+    Course_Video,
+    Course,
+    Payment_Transfer,
+    Rating,
+    Skill,
+    Subscription_Skill,
+    Subscription,
+    User_Course_Favorite,
+    User,
   ],
 });
 
