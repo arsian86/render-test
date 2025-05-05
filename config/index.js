2b4c66da9c9a99193de57eecf2865bb20f46fae9
Arsian Kuo <arsiankuo86@gmail.com>
2025-05-06 00:57:56 +0800

    env

--- config/index.js
+++ config/index.js
@@ -1,43 +1,46 @@
-const dotenv = require('dotenv')//這隻index.js主要是用來讀取.env檔案的 處理環境變數的
-//config是針對環境變數的統一管理
-const result = dotenv.config()
-const db = require('./db')
-const web = require('./web')
-const secret = require('./secret')
+//這隻index.js主要是用來讀取.env檔案的 處理環境變數的
 
-if (result.error) {
-  throw result.error
+//NODE_ENV在 Render 後台預設為 "production"
+if (process.env.NODE_ENV !== "production") {
+	// 如果不是 production（例如你在自己電腦開發）
+	// 才去把 .env 裡的內容讀進來
+	require("dotenv").config();
 }
+//config是針對環境變數的統一管理
+const db = require("./db");
+const web = require("./web");
+const secret = require("./secret");
+
 const config = {
-  db,
-  web,
-  secret
-}
+	db,
+	web,
+	secret,
+};
 
 class ConfigManager {
-  /**
-   * Retrieves a configuration value based on the provided dot-separated path.
-   * Throws an error if the specified configuration path is not found.
-   *
-   * @param {string} path - Dot-separated string representing the configuration path.
-   * @returns {*} - The configuration value corresponding to the given path.
-   * @throws Will throw an error if the configuration path is not found.
-   */
+	/**
+	 * Retrieves a configuration value based on the provided dot-separated path.
+	 * Throws an error if the specified configuration path is not found.
+	 *
+	 * @param {string} path - Dot-separated string representing the configuration path.
+	 * @returns {*} - The configuration value corresponding to the given path.
+	 * @throws Will throw an error if the configuration path is not found.
+	 */
 
-  static get (path) {
-    if (!path || typeof path !== 'string') {
-      throw new Error(`incorrect path: ${path}`)
-    }
-    const keys = path.split('.')
-    let configValue = config
-    keys.forEach((key) => {
-      if (!Object.prototype.hasOwnProperty.call(configValue, key)) {
-        throw new Error(`config ${path} not found`)
-      }
-      configValue = configValue[key]
-    })
-    return configValue
-  }
+	static get(path) {
+		if (!path || typeof path !== "string") {
+			throw new Error(`incorrect path: ${path}`);
+		}
+		const keys = path.split(".");
+		let configValue = config;
+		keys.forEach((key) => {
+			if (!Object.prototype.hasOwnProperty.call(configValue, key)) {
+				throw new Error(`config ${path} not found`);
+			}
+			configValue = configValue[key];
+		});
+		return configValue;
+	}
 }
 
-module.exports = ConfigManager
+module.exports = ConfigManager;
--- db/data-source.js
+++ db/data-source.js
@@ -1,13 +1,14 @@
 const { DataSource } = require("typeorm");
-require("dotenv").config();
+const db_config = require("../config/db");
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: db_config.host,
+  port: db_config.port,
+  username: db_config.username,
+  password: db_config.password,
+  database: db_config.database,
+  ssl: db_config.ssl,
   synchronize: false, //開發時 true，部署時請改為 false 並使用 migration
   logging: true,
   entities: [
